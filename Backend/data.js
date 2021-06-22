a657dc2becc3dc5a144076c0f5ebf018545048e7
MOHAMMED TALHA <atalhahb@gmail.com>
2021-06-22 21:07:56 +0530

    Signed-off-by: MOHAMMED TALHA <atalhahb@gmail.com>

--- Backend/data.js
+++ Backend/data.js
@@ -1,59 +1,78 @@
+// import bcrytjs from 'bcrytjs'
 const data={
+    users:[{
+        name:'Saleh',
+        email:'admin@example.com',
+        password: '1234',
+        isAdmin:true
+    },
+    {
+        name:'user',
+        email:'user@example.com',
+        password: '1234',
+        isAdmin:false
+    }
+    ],
     products: [
         {
-            _id:1,
+            // _id:'1',
             name:'Boat 245',
             category:'headset',
-            image:'/image/p2.png',
+            image:'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-255-blue_540x.png?v=1623833046',
             price:120,
             brand:'boat',
             rating:5,
-            numreview:10,
-            descrip:'best headset'
+            numReviews:14,
+            description:'best headset',
+            countInStock:3
         },
         {
-            _id:2,
-            name:'Boat 245',
+            // _id:'2',
+            name:'Boat 255',
             category:'headset',
-            image:'/image/p3.png',
+            image:'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-255-neon_e6de17f7-ed98-4f41-9af5-b024d0a44389_540x.png?v=1623833046',
             price:350,
             brand:'boat',
             rating:4.5,
-            numreview:10,
-            descrip:'best headset'
+            numReviews:10,
+            description:'best headset',
+            countInStock:3
         },
         {
-            _id:3,
+            // _id:'3',
             name:'Boat 265',
             category:'headset',
-            image:'/image/p2.png',
+            image:'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-255-blue_540x.png?v=1623833046',
             price:125,
             brand:'boat',
             rating:4.5,
-            numreview:10,
-            descrip:'best headset'
+            numReviews:10,
+            description:'best headset',
+            countInStock:3
         },
         {
-            _id:4,
-            name:'Boat 235',
+            // _id:'4',
+            name:'Boat 235A',
             category:'headset',
-            image:'/image/p3.png',
+            image:'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-255-neon_e6de17f7-ed98-4f41-9af5-b024d0a44389_540x.png?v=1623833046',
             price:450,
             brand:'boat',
             rating:2,
-            numreview:10,
-            descrip:'best headset'
+            numReviews:5,
+            description:'best headset',
+            countInStock:3
         },
         {
-            _id:5,
-            name:'Boat 235',
+            // _id:'5',
+            name:'Boat 235B',
             category:'headset',
-            image:'/image/p2.png',
+            image:'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-255-red_ea4e742b-865c-485c-9e48-99d3df7a7c99_540x.png?v=1623833046',
             price:450,
             brand:'boat',
             rating:2,
-            numreview:10,
-            descrip:'best headset'
+            numReviews:10,
+            description:'best headset',
+            countInStock:3
         }
 
     ]
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,46 @@
 import express from 'express';
-import data from './data.js';
+import productRouter from './routers/productRouter.js';
+import mongoose from 'mongoose'
+import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 const app=express();
-app.get('/api/products',(req,res)=>{
-    res.send(data.products)
-})
-app.get('/',(req,res)=>{
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/SalehShop', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// app.get('/api/products/:id',(req,res)=>{
+//     const product =data.products.find((x)=> x._id===req.params.id)
+//     if (product){
+//         res.send(product)
+//     }
+//     else {
+//         res.status(404).send({message : 'Product not found'})
+//     }
+    
+// })
+app.use('/api/products', productRouter);
+// app.get('/api/products',(req,res)=>{
+//     res.send(data.products)
+// })
+
+  app.use('/api/users', userRouter);
+  app.use('/api/orders', orderRouter);
+  app.get('/',(req,res)=>{
     res.send('Server is ready')
 }
 );
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+  });
 const port=process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`server at http://localhost:${port}`)
